3e5cb3dbaa2356690d83d72bcb358a444e960e8e
vvsafwan <vvsafwan2002@gmail.com>
2024-02-15 15:38:11 +0530

    component wise splitting

--- src/Content.js
+++ src/Content.js
@@ -1,65 +1,16 @@
-import React, { useState } from "react";
-import { FaTrashAlt } from "react-icons/fa";
+import React from "react";
+import ItemList from "./ItemList";
 
-const Content = () => {
-    const [items, setItems] = useState([
-        {
-            id: 1,
-            checked: true,
-            item: "Item 1",
-        },
-        {
-            id: 2,
-            checked: false,
-            item: "Item 2",
-        },
-        {
-            id: 3,
-            checked: false,
-            item: "Item 3",
-        },
-    ]);
-
-    const handleCheck = (id) => {
-        const listItems = items.map((item) =>
-            item.id === id ? { ...item, checked: !item.checked } : item
-        );
-        setItems(listItems);
-    };
-
-    const handleDelete = (id) => {
-        const listItems = items.filter((item) => item.id !== id);
-        setItems(listItems);
-    };
+const Content = ({items, handleCheck, handleDelete}) => {
 
     return (
         <main>
             {items.length > 0 ? (
-                <ul>
-                    {items.map((item) => (
-                        <li className="item" key={item.id}>
-                            <input
-                                type="checkbox"
-                                checked={item.checked}
-                                onChange={() => handleCheck(item.id)}
-                            />
-                            <label
-                                style={
-                                    item.checked
-                                        ? { textDecoration: "line-through" }
-                                        : null
-                                }
-                            >
-                                {item.item}
-                            </label>
-                            <FaTrashAlt
-                                onClick={() => handleDelete(item.id)}
-                                role="button"
-                                tabIndex={0}
-                            />
-                        </li>
-                    ))}
-                </ul>
+                <ItemList 
+                    items={items}
+                    handleCheck={handleCheck}
+                    handleDelete={handleDelete}
+                />
             ) : (
                 <p
                     style={{
