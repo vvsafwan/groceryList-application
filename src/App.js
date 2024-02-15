5f17f51a2f58e7689611ea039aa57d86aa8b3df4
vvsafwan <vvsafwan2002@gmail.com>
2024-02-15 16:23:46 +0530

    search feature

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Content from "./Content";
 import Footer from "./Footer";
 import Header from "./Header";
 import AddItem from "./AddItem";
+import SearchItem from "./SearchItem";
 
 function App() {
 
@@ -26,6 +27,7 @@ function App() {
     ]);
 
     const [newItem, setNewItem] = useState('');
+    const [search, setSearch] = useState('');
 
     const addItem = (item) => {
         const id = items[items.length-1].id + 1;
@@ -61,8 +63,12 @@ function App() {
                 setNewItem={setNewItem}
                 handleSubmit={handleSubmit}
             />
+            <SearchItem
+                search={search}
+                setSearch={setSearch}
+            />
             <Content  
-				items={items}
+				items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
 				handleCheck={handleCheck}
 				handleDelete={handleDelete}
 			/>
---
+++ src/SearchItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SearchItem = ({search, setSearch}) => {
+  return (
+    <form className='searchForm' onSubmit={(e) => e.preventDefault()}>
+        <label htmlFor="search">Search</label>
+        <input type="text" id='search' role='searchbox' placeholder='Search Item' value={search} onChange={(e) => setSearch(e.target.value)} />
+    </form>
+  )
+}
+
+export default SearchItem
